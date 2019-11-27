1ef8e6432c146044cab6030c1257194ac08bf673
alijavanmard <ali.javanmard14@gmail.com>
2019-11-27 21:39:58 +0330

    first eddit2

--- src/Plant.java
+++ src/Plant.java
@@ -1,2 +1,55 @@
 public class Plant {
+
+    private int sun;
+    private int rest;
+    private int life;
+    private String comment;
+    private int speed;
+
+    public int getSun() {
+        return sun;
+    }
+
+    public void setSun(int sun) {
+        this.sun = sun;
+    }
+
+    public int getRest() {
+        return rest;
+    }
+
+    public void setRest(int rest) {
+        this.rest = rest;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public Plant(int sun, int rest, int life, String comment) {
+        this.sun = sun;
+        this.rest = rest;
+        this.life = life;
+        this.comment = comment;
+    }
 }
---
+++ src/Zombie.java
@@ -0,0 +1,12 @@
+public class Zombie {
+
+    private int life;
+    private int speed;
+    private String comment;
+
+    public Zombie(int life, int speed, String comment) {
+        this.life = life;
+        this.speed = speed;
+        this.comment = comment;
+    }
+}
