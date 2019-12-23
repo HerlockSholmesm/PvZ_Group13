5059e705d522688f46833d240b4f60dbb247bfc9
alijavanmard <ali.javanmard14@gmail.com>
2019-12-23 14:37:57 +0330

    javna 1.4

--- src/Collection.java
+++ src/Collection.java
@@ -3,24 +3,25 @@ import java.util.ArrayList;
 public class Collection {
     private ArrayList<Card> hand;
     private ArrayList<Card> collection;
-
+    private Shop shop;
     public ArrayList<Card> showHand() {
         return hand;
     }
 
-    public void addToHand(Card card) {
-        hand.add(card);
+    public void addToHand(String name) {
+        ArrayList<Card> cards=shop.getCards();
+        for (Card card :shop.getCards()) {
+            if(card.getName().equals(name)){
+                hand.add(card);
+            }
+        }
     }
 
     public ArrayList<Card> showCollection() {
-        return collection;
+       return shop.getCollection();
     }
 
-    public void setCollection(ArrayList<Card> collection) {
-        this.collection = collection;
-    }
-
-    public Collection() {
-      //  collection.add( Plant Peashooter)
+    public Collection(Shop shop) {
+      this.shop=shop;
     }
 }
