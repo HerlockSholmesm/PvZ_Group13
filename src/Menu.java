91d1492cc4b74b9a5e768709093e8d8f0d13dab2
Abolfazl Rahimi <45298966+abolfazlrahimi@users.noreply.github.com>
2019-12-24 16:54:35 +0330

    menu and commands are being written

--- src/Menu.java
+++ src/Menu.java
@@ -1,18 +1,14 @@
 import java.util.ArrayList;
 
 public interface Menu {
-    Menu parentMenu = null;
-    ArrayList<Menu> childMenus = null;
+    ArrayList<Menu> childMenus = new ArrayList<>();
 
     void help();
     void exit(Menu menu);
 
 }
 
-
-class MainMenu implements Menu{
-
-
+class ShopMenu implements Menu{
 
     @Override
     public void help() {
@@ -21,48 +17,113 @@ class MainMenu implements Menu{
 
     @Override
     public void exit(Menu menu) {
-        menu = this.parentMenu;
 
+    }
+}
+
+class MainMenu implements Menu{
+    Menu parentMenu = new LoginMenu();
+    public MainMenu(){
+        parentMenu = new LoginMenu();
+        this.childMenus.add(new PlayMenu());
+        this.childMenus.add(new ProfileMenu());
+        this.childMenus.add(new ShopMenu());
+    }
+
+    public void goToPlayMenu(Menu menuPtr){
+        menuPtr = this.childMenus.get(0);
+    }
+
+    public void goToProfileMenu(Menu menuPtr){
+        menuPtr = this.childMenus.get(1);
+    }
+    public void goToShopMenu(Menu menuPtr){
+        menuPtr = this.childMenus.get(2);
+    }
+
+    @Override
+    public void help() {
+        System.out.println("help : showing commands" +
+                "play : go to play menu" +
+                "profile : go to profile menu" +
+                "shop : go to shop menu");
+    }
+
+    @Override
+    public void exit(Menu menuPtr) {
+        menuPtr = this.parentMenu;
     }
 }
 
 class LoginMenu implements Menu{
 
+    Menu parentMenu = null;
+    public LoginMenu(){
+        parentMenu = null;
+        this.childMenus.add(new MainMenu());
+        this.childMenus.add(new LeaderBoard());
+    }
+
+    public void goToMainMenu(Menu menuPtr){
+        menuPtr = this.childMenus.get(0);
+    }
+
+    public void goToLeaderBoardMenu(Menu menuPtr){
+        menuPtr = this.childMenus.get(1);
+    }
+
     @Override
     public void help() {
+        System.out.println("help : showing commands" +
+                "Create account : to create an account" +
+                "Login : to login" +
+                "Leader board : to show leader Board" +
+                "exit : to exit game");
 
     }
 
     @Override
-    public void exit(Menu menu) {
-
+    public void exit(Menu menuPtr) {
+        try {
+            menuPtr = this.parentMenu;
+            menuPtr.exit(menuPtr);
+        } catch (Exception e){
+            e.getMessage();
+            // TODO: 12/24/2019
+        }
     }
 }
 
 class PlayMenu implements Menu{
 
-    //todo
+    Menu parentMenu = new MainMenu();
+    public PlayMenu(){
+        this.childMenus.add(new CollectionMenu());
+        this.childMenus.add(new RailMenu());
+    }
+
+    public void goToPlayMenu(Menu menuPtr){
+        menuPtr = this.childMenus.get(0);
+    }
+
+    public void goToProfileMenu(Menu menuPtr){
+        menuPtr = this.childMenus.get(1);
+    }
+    public void goToShopMenu(Menu menuPtr){
+        menuPtr = this.childMenus.get(2);
+    }
+
     @Override
     public void help() {
-
+        System.out.println("help : showing commands" +
+                "play : go to play menu" +
+                "profile : go to profile menu" +
+                "shop : go to shop menu");
     }
 
     @Override
-    public void exit(Menu menu) {
-
-    }
-}
-
-class CollectionMenu implements Menu{
-
-    @Override
-    public void help() {
-
-    }
-
-    @Override
-    public void exit(Menu menu) {
-
+    public void exit(Menu menuPtr) {
+        menuPtr = this.parentMenu;
     }
 }
 
