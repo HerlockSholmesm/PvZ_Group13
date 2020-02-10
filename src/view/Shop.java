4c61730c1309983018657b73f4f587ad8eaef08f
alijavanmard <ali.javanmard14@gmail.com>
2020-02-10 04:04:41 +0330

    graphic menu-changing shop2

--- src/view/Shop.java
+++ src/view/Shop.java
@@ -17,6 +17,7 @@ import model.Plant;
 import model.shootingPlant;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Shop extends Application {
 
@@ -26,30 +27,38 @@ public class Shop extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Group root = new Group();
-        Scene scene = new Scene(root, 900, 800);
+        ArrayList<Plant> plants = model.Shop.getAllPlants();
+        ImageView imageView ;
         GridPane gridPane = new GridPane();
         gridPane.setVgap(30);
         gridPane.setHgap(30);
-        Image image = new Image("Plants vs Zombies/Snow Pea/Snow-Pea.webp");
+        System.out.println(plants.size());
+        for (int i=0;i<plants.size() ;i++){
+            imageView=new ImageView(plants.get(i).getCardImage());
+            gridPane.add(imageView,i,i);
+
+        }
+        Group root = new Group();
+        Scene scene = new Scene(root, 900, 800);
+        //Image image = new Image("Plants vs Zombies/Snow Pea/Snow-Pea.webp");
         Image image2 = new Image("Plants vs Zombies/Snow Pea/Snow-Pea.webp");
-        ImageView imageView = new ImageView(image);
+       // ImageView imageView = new ImageView(image);
         int money = 100;
         //ImageView imageView2=new ImageView(image2);
         Plant plant = new shootingPlant("Repeater", 3, 2, 3, 4, 4,
                 new Image("Plants vs Zombies/Repeater/1769829-plant_peashooter_thumb.webp"));
         ImageView imageView2 = new ImageView(plant.getImage());
-        gridPane.add(imageView, 1, 1);
+        //gridPane.add(imageView, 1, 1);
         gridPane.add(imageView2, 1, 2);
       //  Text title1=toText(imageView2,100,20);
-        imageView.setOnMouseClicked(event -> {
+     /*   imageView.setOnMouseClicked(event -> {
             Text text = new Text(String.valueOf(money - plant.getPrice()));
             text.setFont(Font.font("Verdana", 50));
             text.setTranslateX(20);
             text.setTranslateY(40);
             text.setId("fancytext");
             root.getChildren().add(text);
-        });
+        });*/
         root.getChildren().add(gridPane);
         primaryStage.setScene(scene);
         primaryStage.show();
